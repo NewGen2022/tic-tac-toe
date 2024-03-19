2b2fd22e64479b251a3defbb47a30f4056610612
NewGen2022 <amirgimari@gmail.com>
2024-03-19 18:05:50 +0200

    tic-tac-toe in console (refactor)

--- script.js
+++ script.js
@@ -1,117 +1,135 @@
-const BOARD_SIZE = 9;
-
-let gameBoard = Array(BOARD_SIZE).fill('');
-let currentPlayer = null;
 let gameOver = false;
 
-const winCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6]              // Diagonals
-];
+const Board = () => {
+    const BOARD_SIZE = 9;
+    let board = Array(BOARD_SIZE).fill('');
 
-function createPlayer(playerName, playerOption){
-    let name = playerName;
-    let option = playerOption;
+    const winCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
 
-    const setName = (newName) => playerName = newName;
-    const getName = () => playerName;
-
-    const setOption = (newOption) => playerOption = newOption;
-    const getOption = () => playerOption;
-
-    return {getName, setName, getOption, setOption}
-}
-
-function createPlayers(){
-    const playerName1 = prompt('Enter player 1 name');
-    const playerName2 = prompt('Enter player 2 name');
-
-    const playerOption = prompt('Enter 1 for X OR 0 for O');
-
-    const playerOption1 = (playerOption === '1' ? 'X' : 'O')
-    const playerOption2 = (playerOption1 === 'O' || playerOption1 === '' ? 'X' : 'O')
-
-    const player1 = createPlayer(playerName1, playerOption1)
-    const player2 = createPlayer(playerName2, playerOption2)
-
-    return {player1, player2}
-}
-
-function handleMove(index){
-    if(index >= 0 && index < 9 && gameBoard[index] === ''){
-        gameBoard[index] = currentPlayer.getOption()
-        return true
+    function getBoard(){
+        return board
     }
 
-    return false
-}
-
-function checkTie() {
-    return gameBoard.every(cell => cell !== '');
-}
-
-function checkWin(){
-    if(checkTie()){
-        gameOver = true;
-        console.log("TIE")
-        alert("TIE")
-        return;
+    function displayBoard(){
+        const divider = "\n-----------\n";
+        const rows = board.reduce((acc, cell, index) => {
+            const separator = index % 3 === 0 && index !== 0 ? divider : "";
+            return acc + separator + (cell || " ") + (index % 3 === 2 ? "" : " | ");
+        }, "");
+        console.log(rows);
     }
 
-    for(let combination of winCombinations){
-        const [i, j, k] = combination
-
-        if (gameBoard[i] !== '' && gameBoard[i] === gameBoard[j] && gameBoard[i] === gameBoard[k]) {
-            gameOver = true;
-            console.log(`Player ${currentPlayer.getName()} (${currentPlayer.getOption()}) wins!\nCongratulations`)
-            alert(`Player ${currentPlayer.getName()} (${currentPlayer.getOption()}) wins!\nCongratulations`)
-            return;
+    function displayMove(index, currentPlayer){
+        if(index >= 0 && index < 9 && board[index] === ''){
+            board[index] = currentPlayer.mark
+            return true
         }
+    
+        return false
     }
-}
 
-function displayBoard() {
-    const rows = [];
-    for (let i = 0; i < 9; i += 3) {
-        const row = gameBoard.slice(i, i + 3).join(' | ');
-        rows.push(row);
+    function checkTie(){
+        return board.every(cell => cell !== '');
     }
-    const board = rows.join('\n---------\n');
-    console.log(board);
-}
 
-(function game(){
-    const {player1, player2} = createPlayers()
-
-    console.log(player1.getName())
-    console.log(player2.getName())
-
-    console.log(gameBoard)
-
-    currentPlayer = player1.getOption() === "X" ? player1 : player2;
-
-    console.log(currentPlayer.getName())
-
-    while(!gameOver){
-        const move = parseInt(prompt(`Enter cell you want to put a X/O
-        [1 2 3]
-        [4 5 6]
-        [7 8 9]`))
-
-        if(handleMove(move-1)){
-            checkWin()
-            currentPlayer = currentPlayer === player1 ? player2 : player1;
-        }
-
-        alert(`
-        ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
-        -----------
-        ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
-        -----------
-        ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
-        `)
+    function checkWin(currentPlayer){
         
-        displayBoard();
+        for(let combination of winCombinations){
+            const [i, j, k] = combination
+            
+            if (board[i] !== '' && board[i] === board[j] && board[i] === board[k]) {
+                gameOver = true;
+                console.log(`Player ${currentPlayer.name} (${currentPlayer.mark}) wins!\nCongratulations`)
+                alert(`Player ${currentPlayer.name} (${currentPlayer.mark}) wins!\nCongratulations`)
+                return;
+            }
+
+            if(checkTie()){
+                gameOver = true;
+                console.log("TIE")
+                alert("TIE")
+                return;
+            }
+        }
     }
+
+    return { displayBoard, displayMove, checkTie, checkWin, getBoard }
+}
+
+const Players = (() => {
+    let players = [];
+    let currentPlayerIndex = 0;
+
+    function createPlayer(playerName, playerMark) {
+        return {
+            name: playerName,
+            mark: playerMark
+        };
+    }
+
+    // Function to create players
+    function createPlayers() {
+        for (let i = 0; i < 2; i++) {
+            const playerName = prompt(`Enter player ${i + 1} name`);
+            const playerMark = i === 0 ? prompt(`Enter mark for Player ${playerName} (X or O)`).toUpperCase() : players[0].mark === 'X' ? 'O' : 'X';
+            players.push(createPlayer(playerName, playerMark));
+        }
+        currentPlayerIndex = players[1].mark === 'X' ? 1 : 0;
+        return players;
+    }
+
+    // Function to switch the current player
+    function switchPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+        return players[currentPlayerIndex];
+    }
+
+    // Function to get the current player
+    function getCurrentPlayer() {
+        return players[currentPlayerIndex];
+    }
+
+    return { createPlayers, switchPlayer, getCurrentPlayer };
+})();
+
+const Game = (() => {
+    function start(){
+        const gameBoard = Board();
+        const [ player1, player2 ] = Players.createPlayers();
+    
+        let currentPlayer = Players.getCurrentPlayer()
+    
+        console.log("Player 1 name:", player1.name)
+        console.log("Player 2 name:", player2.name)
+        gameBoard.displayBoard()
+        console.log("Current player name:", currentPlayer.name)
+    
+        while(!gameOver){
+            const move = parseInt(prompt(`Enter cell you want to put ${currentPlayer.mark}
+            [1 2 3]
+            [4 5 6]
+            [7 8 9]
+            ${gameBoard.getBoard()[0]} | ${gameBoard.getBoard()[1]} | ${gameBoard.getBoard()[2]}
+            -----------
+            ${gameBoard.getBoard()[3]} | ${gameBoard.getBoard()[4]} | ${gameBoard.getBoard()[5]}
+            -----------
+            ${gameBoard.getBoard()[6]} | ${gameBoard.getBoard()[7]} | ${gameBoard.getBoard()[8]}
+            `))
+    
+            if(gameBoard.displayMove(move - 1, currentPlayer)){
+                gameBoard.checkWin(currentPlayer)
+                currentPlayer = Players.switchPlayer()
+            }
+            
+            gameBoard.displayBoard();
+        }
+    }
+
+    return { start };
 })()
+
+Game.start()
